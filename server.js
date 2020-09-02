b7d2c429ddacf6a6bf2d0ba802efcd181c202e8d
José Fernando <jose.gomezm12@gmail.com>
2020-09-02 15:30:21 -0500

    queries

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const { ApolloServer, gql } = require('apollo-server-express')
 const cors = require('cors')
 const dotEnv = require('dotenv')
 
+const {tasks, users} = require('./constants')
+
 //env variables
 dotEnv.config()
 
@@ -14,9 +16,45 @@ app.use(cors())
 const typeDefs = gql`
   type Query {
     greetings: String
+    tasks: [Task!]
+    task(id: ID!): Task
+    users: [User!]
+    user(id: ID!): User
+  }
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    tasks: [Task!]
+  }
+
+  type Task {
+    id: ID!
+    name: String!
+    completed: Boolean!
+    user: User!
+
   }
 `
-const resolvers = {}
+const resolvers = {
+  Query: {
+    greetings: () => "Hello",
+    tasks: () => tasks,
+    task: (_, {id}) => tasks.find(task => task.id === id),
+    users: () => users,
+    user: (_, {id}) => users.find(user => user.id === id)
+  },
+  Task: {
+    user: ({userId}) => {
+      return users.find( user => user.id === userId)
+    }
+  },
+  User: {
+    tasks: ({id}) => {
+      return tasks.filter(task => task.userId === id)
+    }
+  }
+}
 
 const apolloServer = new ApolloServer({
   typeDefs,
