a5aba92a4203f80bdd53e5843b7940f8b04cf939
José Fernando <jose.gomezm12@gmail.com>
2020-09-06 16:04:39 -0500

    login with jwt

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const {users, tasks} = require('../constants')
 const User = require('../models/user')
@@ -23,7 +24,30 @@ module.exports = {
       } catch(error) {
         throw error
       }
-    }
+    },
+    login: async (_, { input }) => {
+      try {
+        const user = await User.findOne({email: input.email})
+
+        if(!user) {
+          throw new Error('Incorrect email')
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(input.password, user.password)
+        
+        if(!isPasswordCorrect) {
+          throw new Error('Incorrect password')
+        } 
+
+        const secret = process.env.JWT_SECRET || 'DefaultSecretForThisApp'
+        const token = jwt.sign({ email: user.email }, secret, { expiresIn: '1d' })
+
+        return { token }
+      } catch(error) {
+        console.log(error)
+        throw error
+      }
+     }
   },
   User: {
     tasks: ({id}) => {
